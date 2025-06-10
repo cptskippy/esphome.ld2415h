5068dd452035bc70703d83c24227c1eb4a764b3f
CptSkippy <cptskippy@users.noreply.github.com>
2025-06-09 17:22:35 -0700

    fixing type

--- components/ld2415h/sensor/__init__.py
+++ components/ld2415h/sensor/__init__.py
@@ -10,8 +10,8 @@ from esphome.const import (
 )
 from .. import ld2415h_ns, LD2415HComponent, CONF_LD2415H_ID
 
-LD2415HSpeedSensor = ld2415h_ns.class_("LD2415HSpeedSensor", sensor.Sensor, cg.Component)
-LD2415HVelocitySensor = ld2415h_ns.class_("LD2415HVelocitySensor", sensor.Sensor, cg.Component)
+LD2415HSpeedSensor = ld2415h_ns.class_("SpeedSensor", sensor.Sensor, cg.Component)
+LD2415HVelocitySensor = ld2415h_ns.class_("VelocitySensor", sensor.Sensor, cg.Component)
 
 ICON_SPEEDOMETER = "mdi:speedometer"
 
